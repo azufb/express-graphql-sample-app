a70239071c39eba9ea3c0a9a2fa9f9aa36e71c49
azufb <curiousazunas@gmail.com>
2023-09-18 12:53:02 +0900

    mutationの追加など

--- index.ts
+++ index.ts
@@ -4,17 +4,59 @@ import { GraphQLSchema, buildSchema } from 'graphql';
 const app: express.Express = express();
 const port: number = 3000;
 
+let sampleUsers = [
+  {
+    id: 1,
+    name: 'user_1',
+    comment: 'hello',
+  },
+  {
+    id: 2,
+    name: 'user_2',
+    comment: 'hello world',
+  },
+  {
+    id: 3,
+    name: 'user_3',
+    comment: 'bye',
+  },
+];
+
 // APIのデータ型の集合をスキーマと言う
 const schema: GraphQLSchema = buildSchema(`
+  type SampleUser {
+    id: Int,
+    name: String,
+    comment: String
+  }
+
   type Query {
     hello(name: String): String
+    getSampleUsers: [SampleUser]
+  }
+
+  type Mutation {
+    postSampleUser(name: String, comment: String): SampleUser
   }
 `);
 
 // データを操作する処理を記述するのがリゾルバ
 const root = {
-  hello: (args: { name: string }) => {
-    return `Hello, ${args.name}!`;
+  hello: () => {
+    return `Hello!`;
+  },
+  getSampleUsers: () => {
+    return sampleUsers;
+  },
+  postSampleUser: (args: { name: string; comment: string }) => {
+    const id = sampleUsers.length === 0 ? 1 : sampleUsers.length + 1;
+    const addUser = {
+      id: id,
+      name: args.name,
+      comment: args.comment,
+    };
+    sampleUsers = [...sampleUsers, addUser];
+    return addUser;
   },
 };
 
